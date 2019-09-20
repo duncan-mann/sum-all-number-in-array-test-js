e909033b4a117c0845d8b72debe86b4057f91a55
Duncan Mann <duncan.s.mann@gmail.com>
2019-09-20 18:35:38 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
-}
+  let sum = 0;
 
-module.exports = sumItems;
+  for (item of array) {
+    if (Array.isArray(item) && item.length > 0) { 
+      sum += sumItems(item);
+    } else if (!Array.isArray(item)) {
+      sum += item;
+    }
+  }
+  return sum;
+}
+console.log(sumItems([[1, 2, [[3], 4]], 5, []])) 
+
+// module.exports = sumItems;
